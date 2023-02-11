2ab2e31c823b64b5e32a35aa0c0b40d5755ac0e5
RubenAlamo <113826388+RubenAlamo@users.noreply.github.com>
2023-02-11 19:37:55 +0100

    Update Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class Ejercicio4 {
-    //declaracion de variables
+    //declaración de variables
     public static double distancia;
     public static double tiempo;
     public static double velocidad;
-    //declaracion de la clase Scanner
+    //declaración de la clase Scanner
     public static Scanner sc = new Scanner(System.in);
     //metodo main
     public static void main(String[] args) {
-        //asignacion de valores a las variables
+        //asignación de valores a las variables
         System.out.println("Introduce la distancia recorrida en km: ");
         distancia = sc.nextDouble();
         System.out.println("Introduce el tiempo empleado en horas: ");
         tiempo = sc.nextDouble();
-        //calculo de la velocidad
+        //cálculo de la velocidad
         velocidad = distancia / tiempo;
         //imprimir la velocidad
         System.out.println("La velocidad media es: " + velocidad + " km/h");
